c671c236983f6edf613fd6175be731bc8b12c0d4
RD-30 <rajidharma.24@gmail.com>
2025-10-07 15:16:19 +0100

    final

--- index.js
+++ index.js
@@ -13,21 +13,22 @@ const close = document.querySelector('.close-icon')
 
 const videoList = [video1, video2, video3, video4, video5, video6];
 
-videoList.forEach(function(video){
-    video.addEventListener('mouseover', function(){
-        video.play().then(() =>{
-             hoverSign.classList.add("active");
-        })
-        .catch((err) => {
-            console.warn("Autoplay blocked:", err);
-        });
-    });
-    
-    video.addEventListener('mouseout', function(){
-        video.pause();
-         hoverSign.classList.remove("active")
-    })
-})
+videoList.forEach(function(video) {
+  video.muted = true;
+  video.playsInline = true;
+  video.setAttribute('preload', 'metadata');
+  video.setAttribute('loop', true);
+
+  // Click-to-play for mobile
+  video.addEventListener('click', function () {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
+
 
 menu.addEventListener("click", function(){
     sideBar.classList.remove("close-sidebar")
